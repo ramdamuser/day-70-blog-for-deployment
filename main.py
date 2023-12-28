ecbc8ad4d54a1d3e0363f6b2a8af37fd12fd14d6
ramdamuser <ramdamuser@gmail.com>
2023-12-28 12:48:50 +1100

    change main.py

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ ckeditor = CKEditor(app)
 Bootstrap(app)
 
 ##CONNECT TO DB
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///posts.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DB_URI", "sqlite:///posts.db")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 # db.init_app(app)
