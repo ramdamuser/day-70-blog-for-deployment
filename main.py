5ece0b6f987270e053b61e055b289998ab1f85c0
ramdamuser <ramdamuser@gmail.com>
2023-12-28 18:40:27 +1100

    Complete Project

--- main.py
+++ main.py
@@ -143,7 +143,7 @@ def show_post(post_id):
         else:
             flash("You need to login or register to comment.")
             return redirect(url_for("login"))
-        return redirect(url_for("show_post"))
+        return redirect(url_for("show_post", id=post_id))
     return render_template("post.html", post=requested_post, form=form, comments=all_comments, gravatar=gravatar)
 
 
