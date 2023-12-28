7ee3335d0dce0ddddd9dac188311a579ec61ca41
ramdamuser <ramdamuser@gmail.com>
2023-12-28 17:23:12 +1100

    Complete Project

--- main.py
+++ main.py
@@ -33,7 +33,7 @@ class User(UserMixin, db.Model):
     __tablename__ = "users"
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(100), unique=True)
-    password = db.Column(db.Text)
+    password = db.Column(db.Text(1000))
     name = db.Column(db.String(100))
     posts = relationship("BlogPost", back_populates="author")
     comments = relationship("Comment", back_populates="author")
